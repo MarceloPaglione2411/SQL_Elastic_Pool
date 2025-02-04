e717c291fbea053e5f372e6421c603f933d2d93e
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-04 16:34:21 -0300

    Update variable.tf

--- sql/variable.tf
+++ sql/variable.tf
@@ -1,1 +1,54 @@
+variable "rg_name" {
+    description = "Nome RG NAME"
+    type = string
+}
 
+variable "rg_location" {
+    description = "LOCATION RG NAME"
+    type = string
+}
+
+variable "sql_server_name" {
+    description = "Nome SQL SERVER NAME"
+    type = string
+}
+
+variable "sql_server_version" {
+    description = "Nome SQL SERVER VERSION"
+    type = string
+}
+
+variable "login_mssql-server" {
+    description = "LOGIN SQL SERVER"
+    type = string
+}
+
+variable "password_mssql-server" {
+    description = "PASSWORD SQL SERVER"
+    type = string
+}
+
+variable "mssql_elastic_name" {
+    description = "SQL ELASTIC NAME"
+    type = string
+}
+
+variable "sku_elastic" {
+    description = "SKU ELASTIC"
+    type = string
+}
+
+variable "tier_elastic" {
+    description = "TIER ELASTIC SQL"
+    type = string
+}
+
+variable "sql_database_name1" {
+    description = "DATABASE NAME"
+    type = string
+}
+
+variable "sql_database_name2" {
+    description = "DATABASE NAME"
+    type = string
+}
